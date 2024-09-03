5005155cfbceeed83f7ed2f83fbe0898b93196e1
bcjaeger <byron.jaeger@gmail.com>
2024-09-03 09:30:34 -0400

    initialize package section

--- _targets.R
+++ _targets.R
@@ -20,7 +20,8 @@ entries_init_tar <- tar_target(entries_init, command = read_csv(entries_file))
 entries_tar <- tar_target(entries, command = {
   entries_init %>%
     bind_papers(scholar_pubs) %>%
-    bind_abstracts(scholar_pubs)
+    bind_abstracts(scholar_pubs) %>%
+    bind_rpacks(scholar_pubs)
 })
 
 skills_file_tar <- tar_target(skills_file,
